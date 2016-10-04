95240fb989731002fa738990fe23b7eb60b69ee9
fuyi1215 <fuyi1215@msn.com>
2016-10-04 09:31:21 -0400

    Add files via upload

--- LibraryInfoPage.xaml.cs
+++ LibraryInfoPage.xaml.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Xamarin.Forms.Maps;
 using Xamarin.Forms;
 
 namespace Library
 {
 	public partial class LibraryInfoPage : ContentPage
 	{
-		public LibraryInfoPage()
+		private LibInfoViewModel viewModel;
+		public LibraryInfoPage(libInfo lib)
 		{
 			InitializeComponent();
+			BindingContext = viewModel = new LibInfoViewModel(lib, this);
+		}
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			var position = new Position(viewModel.lib.Latitude,viewModel.lib.Longitude); // Latitude, Longitude
+			var pin = new Pin
+			{
+				Type = PinType.Place,
+				Position = position,
+				Label = viewModel.lib.Name,
+				Address = viewModel.lib.StreetAddress
+			};
+			MyMap.Pins.Add(pin);
+
+			MyMap.MoveToRegion(
+				MapSpan.FromCenterAndRadius(
+					position, Distance.FromMiles(.2)));
+
 		}
 	}
+
 }
